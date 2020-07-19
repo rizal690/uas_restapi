614ee6310551da23195f6ccd0624fd8db178bf58
rizal690 <rizalhendri935@outlook.com>
2020-07-19 15:24:49 +0700

    berhasil registrasi

--- Documents/uas_restapi/server.js
+++ Documents/uas_restapi/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var morgan = require('morgan');
 const app = express();
 
 
@@ -7,10 +9,15 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(morgan('dev'));
+
 //panggil routes
 var routes = require('./routes');
 routes(app);
 
+//daftarkan menu routes dari index
+app.use('/auth', require('./middleware'));
+
 app.listen(3000, () => {
-    console.log(`Server started on port`);
+    console.log(`Server started on port 3000`);
 });
